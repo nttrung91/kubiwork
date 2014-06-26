505edb3ee6ec1cbe8faf0a1c66c5185742245ec1
Kubi <trungn3@uci.edu>
2014-06-25 21:24:10 -0700

    Navigation now works on larger screen

--- js/global.js
+++ js/global.js
@@ -5,7 +5,8 @@
 var $menu = $(".nav-menu"), // The nav menu that list all pages
     $menuLink = $(".menu-link"), // The menu link btn
     $badge = $(".kubi-badge"), // The logo
-    $siteWrap = $(".site-wrap"); // The site wrapper
+    $siteWrap = $(".site-wrap"), // The site wrapper
+    $navBar = $(".nav-bar");
 
 // Check to see if Javascript is available
 $('.site-wrap').addClass('js');
@@ -16,16 +17,6 @@ $menuLink.click(function() {
   $menu.toggleClass('isActive');
   $badge.toggleClass('isActive');
   $siteWrap.toggleClass('isActive');
-
-  /*
-   * Activate Velocityjs for smoother animation
-   */
-
-  if($menuLink.is('.isActive'))
-    $menuLink.velocity({ width: "100%" }, { duration: 150 });
-  else
-    // The 55px comes from the menu-link size which comes from $menu-link-size in variable.scss file
-    $menuLink.velocity({ width: "55px" }, { duration: 200 });
 
   return false;
 
