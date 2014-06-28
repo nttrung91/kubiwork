46c3215f235e264cace890e121b24df1a52d6a27
Kubi <trungn3@uci.edu>
2014-06-28 14:12:38 -0700

    Changed Visual Appearance of all pages

--- js/global.js
+++ js/global.js
@@ -22,17 +22,19 @@ $menuLink.click(function() {
 
 });
 
+if(window.location.pathname.indexOf("project") > -1){
+  $(window).scroll(function() {
+    var windowHeight = $(window).height();
+    var bodyOffSet = window.pageYOffset;
 
-$(window).scroll(function() {
-  var windowHeight = $(window).height();
-  var bodyOffSet = window.pageYOffset;
 
+    if(bodyOffSet > windowHeight)
+      $(".nav-bar").addClass("newColor");
+    else
+      $(".nav-bar").removeClass("newColor");
 
-  if(bodyOffSet > windowHeight)
-    $(".nav-bar").addClass("newColor");
-  else
-    $(".nav-bar").removeClass("newColor");
+  });
+}
 
-});
 
 
